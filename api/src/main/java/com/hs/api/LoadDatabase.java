8a6bfff2f1242de1e72ab6a243b7dd18f118f17b
JeanEspindola <jean.espindola@gmail.com>
2019-09-14 09:08:43 +0200

    Preloading data

--- api/src/main/java/com/hs/api/LoadDatabase.java
+++ api/src/main/java/com/hs/api/LoadDatabase.java
@@ -2,23 +2,23 @@ package com.hs.api;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.hs.api.entity.Category;
+import com.hs.api.repository.CategoryRepository;
 
 @Configuration
 @Slf4j
 class LoadDatabase {
 
-  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
   @Bean
-  CommandLineRunner initDatabase(EmployeeRepository repository) {
-    return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-      log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+  CommandLineRunner initDatabase(CategoryRepository category) {
+    return args -> {      
+      log.info("Preloading " + category.save(new Category("Fashion")));
+      log.info("Preloading " + category.save(new Category("Living")));
+      log.info("Preloading " + category.save(new Category("Cosmetics")));
     };
   }
 }
