7ed454c7826974047f624c423a15a0242c10db2b
JeanEspindola <jean.espindola@gmail.com>
2019-09-15 17:21:20 +0200

    Adding Header component.

---
+++ fe/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'react-bootstrap';
+import './Header.scss';
+
+const Header = ({ title }) => (
+  <Row className="header">
+    <Col xs={12}>
+      {title}
+    </Col>
+  </Row>
+);
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
