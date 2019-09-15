e997d7289aade0201cd16feb991cf381550e9204
JeanEspindola <jean.espindola@gmail.com>
2019-09-15 18:19:41 +0200

    Adding constant.

---
+++ fe/src/utils/constants.js
@@ -0,0 +1,9 @@
+// export const API_KEY = 'de99f27226cb42b1993b11f717641745';
+// export const URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+
+// export const LOADING_INDICATOR_SUCCESS = 'LOADING_INDICATOR_SUCCESS';
+
+export const API_BASE_URL = 'http://localhost:8080/';
+export const API_CATEGORIES = 'categories'
+
+export const CATEGORIES_FETCH_SUCCESS = 'CATEGORIES_FETCH_SUCCESS';
