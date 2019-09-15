9da661ab4e7b76f4685144ee852c04142a980987
JeanEspindola <jean.espindola@gmail.com>
2019-09-15 17:16:19 +0200

    Adding scss, adjusting the app.

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Header from './components/header/Header';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app__container">
+    <Header title="Home Shopping Application - Main" />
+    <Row>
+      <Col xs={12}> Header </Col>
+    </Row>
+  </div>
+)
 
 export default App;
