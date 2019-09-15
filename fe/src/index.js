5db490daa7e8ed576640df2b62125e7ea71692ba
JeanEspindola <jean.espindola@gmail.com>
2019-09-15 18:21:30 +0200

    Dispatching the action at loading the app.

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,10 +1,27 @@
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store/configureStore';
+import { loadCategoryList } from './actions/categoryAction';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+store.dispatch(loadCategoryList());
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
