786013ba4ecd7dd7ebd4a502149831ccb3124352
JeanEspindola <jean.espindola@gmail.com>
2019-09-15 21:38:08 +0200

    Adding CrossOrigin support.

--- api/src/main/java/com/hs/api/controller/ProductController.java
+++ api/src/main/java/com/hs/api/controller/ProductController.java
@@ -8,6 +8,7 @@ import java.net.URISyntaxException;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 public class ProductController {
 
