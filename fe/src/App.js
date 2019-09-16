81c277dbf0408634de06e1715271afc3829f03bd
JeanEspindola <jean.espindola@gmail.com>
2019-09-16 20:35:23 +0200

    Connecting App and adding routes.

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Row, Col } from 'react-bootstrap';
 import Header from './components/header/Header';
+import Admin from './scenes/admin/Admin';
+import Shopping from './scenes/shopping/Shopping';
 import './App.scss';
 
 const App = () => (
   <div className="app__container">
-    <Header title="Home Shopping Application - Main" />
+    <Header title="Home Shopping Application" />
     <div className="app__navigation">
-      <Row>
-        <Col sm={12}> Header </Col>
-      </Row>
-      <Row>
-        <Col sm={6}>
-          <Button>Administration</Button>
-        </Col>
-        <Col sm={6}>
-          <Button>Shopping</Button>
-        </Col>
-      </Row>
+      <Router>
+        <Row>
+          <Col sm={12}>
+            <Link to="/" className="app__navigation__router">Home</Link>
+            <Link to="/admin" className="app__navigation__router">Administration</Link>
+            <Link to="/shopping" className="app__navigation__router">Shopping</Link>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Route path="/admin" component={Admin} />
+            <Route path="/shopping" component={Shopping} />
+          </Col>
+        </Row>
+      </Router>
     </div>
   </div>
 )
--- fe/src/components/header/Header.js
+++ fe/src/components/header/Header.js
@@ -4,7 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 import './Header.scss';
 
 const Header = ({ title }) => (
-  <Row className="header">
+  <Row className="header__container">
     <Col xs={12}>
       {title}
     </Col>
