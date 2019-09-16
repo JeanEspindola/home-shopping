35dd562dbd1f1c5e4cde280a926a9e9cfafa2734
JeanEspindola <jean.espindola@gmail.com>
2019-09-17 00:00:40 +0200

    Adding function to handle delete and update category.

--- fe/src/utils/categoryService.js
+++ fe/src/utils/categoryService.js
@@ -1,5 +1,5 @@
 import 'whatwg-fetch';
-import { API_BASE_URL, API_CATEGORIES } from './constants';
+import { API_BASE_URL, API_CATEGORIES, DELETE_PATH } from './constants';
 
 const categoriesUrl = `${API_BASE_URL}${API_CATEGORIES}`;
 
@@ -8,6 +8,22 @@ class CategoryService {
     return fetch(categoriesUrl)
       .then(response => response.json())
       .then(json => json._embedded)
+      .catch(error => error);
+  }
+
+  static async deleteCategory(categoryId) {
+    const url = `${categoriesUrl}/${DELETE_PATH}/${categoryId}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(json => json)
+      .catch(error => error);
+  }
+
+  static async updateCategory(categoryId, objectParam) {
+    const url = `${categoriesUrl}/${DELETE_PATH}/${categoryId}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(json => json)
       .catch(error => error);
   }
 }
---
+++ fe/src/utils/dataHelper.js
@@ -0,0 +1,3 @@
+export const removeObjectFromArray = (list, objectId) => {
+  return list;
+}
